0665438107f395b040c83a80c4c3d18974fb976b
ljulbrich <150336150+ljulbrich@users.noreply.github.com>
2024-03-05 19:50:20 +1100

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,3 +1,17 @@
+-- Database: crowdfunding_db
+
+-- DROP DATABASE IF EXISTS crowdfunding_db;
+
+CREATE DATABASE crowdfunding_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Australia.1252'
+    LC_CTYPE = 'English_Australia.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
 -- Creating tables first
 CREATE TABLE IF NOT EXISTS category (
 	category_id VARCHAR(255) PRIMARY KEY NOT NULL,
